c262097c35d005003c944e4253a7ddce716dd4f7
Elton Khaemba <ekhaemba@udel.edu>
2018-03-21 16:10:14 -0400

    Filter script with the click library so it can work on mac and windows

--- filter.py
+++ filter.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import os
 import sys
+import click
+from pathlib import Path
 
 #Cmd Line:  python <location of this file> <name of tub> <color> <fill or tint>
 #Ex:        python C:\Users\kaleb\PycharmProjects\SeniorDesign\filter.py tub_1_18-03-04 r f
@@ -50,44 +52,35 @@ def filterImage(image,color,tint):
 
     return image
 
-
-def filterTub(tubName,color, tint):
-
-    absoluteTubName = directory + "\\" + tubName + "\\" + tubName
-    allFiles = os.listdir(absoluteTubName)
-
-    newTubName = tubName + "Filtered"
-    newAbsoluteTubName = directory + "\\" + newTubName + "\\" + newTubName
-
-    if not os.path.exists(newAbsoluteTubName):
-        os.makedirs(newAbsoluteTubName)
-
-    for file in allFiles:
-
-        extension = file.split(".")[1]
-
-        if extension == "jpg":
-
-            im = Image.open(absoluteTubName + "\\" + file)
-
+@click.command()
+@click.argument('tubname', type=click.Path(exists=True, dir_okay=True, file_okay=False, readable=True))
+@click.argument('dst', type=click.Path(exists=True, dir_okay=True, file_okay=False, writable=True))
+@click.argument('color', type=click.Choice(['r','g','b']))
+@click.argument('tint', type=click.Choice(['t','f']))
+def filterTub(tubname, dst, color, tint):
+    #The location of the tub images
+    tubPath = Path(tubname)
+    #The location where they will be saved
+    destination = Path(dst)
+    newTubName = tubPath.name + "_filtered"
+    newTubPath = destination / newTubName
+    newTubPath.mkdir(exist_ok=True, parents=True)
+    
+    with click.progressbar(tubPath.glob('*.jpg')) as tub_images:
+        #Iterate through all of the jpgs
+        for file in tub_images:
+            extension = file.name.split(".")[1]
+            im = Image.open(file.absolute())
             im = filterImage(im,color,tint)
-
-            im.save(newAbsoluteTubName + "\\" + file)
-
-
-        else:
-
-            os.rename(absoluteTubName + "\\" + file, newAbsoluteTubName + "\\" + file)
+            new_image_path = Path(newTubPath / file.name)
+            im.save(new_image_path.absolute())
 
 
 if __name__ == "__main__":
-
-    args = sys.argv
-    tubName = args[1]
-    color = args[2]
-    tint = args[3]
-
-    filterTub(tubName, color, tint)
-
-
-
+#
+#     # args = sys.argv
+#     # tubName = args[1]
+#     # color = args[2]
+#     # tint = args[3]
+    filterTub()
+#     #filterTub(tubName, color, tint)
